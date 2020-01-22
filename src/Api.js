db8055d56fc44d7264e43ffdc0bb79245fc56e7e
Ismael Nascimento 'Ash <contato@ismaelnascimento.com>
2020-01-22 02:04:51 -0300

    [UPDATED] Constants.js

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import * as Constants from './Constants';
 
-axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/';
+axios.defaults.baseURL = Constants.API_BASE_URL;
 
 export default axios;
--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,3 @@
+export const API_BASE_URL = 'https://firestore.googleapis.com/v1/';
 export const PROJECT_NAME = 'react-crud-simple-api';
 export const USERS_COLLETICTION_NAME = 'users';
